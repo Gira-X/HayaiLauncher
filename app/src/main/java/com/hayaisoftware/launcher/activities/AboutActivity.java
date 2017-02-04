1ecb115d8bcf53944dd103945c53bcc2bea0989b
Dmytro Butemann <dmytro.butemann@gmail.com>
2017-02-04 13:46:24 +0100

    Remove 'Open keyboard automatically' from settings

--- app/src/main/java/com/hayaisoftware/launcher/activities/AboutActivity.java
+++ app/src/main/java/com/hayaisoftware/launcher/activities/AboutActivity.java
@@ -15,41 +15,38 @@
 
 package com.hayaisoftware.launcher.activities;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
 
-import com.hayaisoftware.launcher.BuildConfig;
-import com.hayaisoftware.launcher.R;
+import com.hayaisoftware.launcher.*;
 
 public class AboutActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
-        TextView aboutTextView=((TextView) findViewById(R.id.about_textview));
-        aboutTextView.setText(aboutTextView.getText().toString().replace("[APP_VERSION]",
-                BuildConfig.VERSION_NAME));
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+		TextView aboutTextView = ((TextView) findViewById(R.id.about_textview));
+		aboutTextView.setText(aboutTextView.getText().toString().replace("[APP_VERSION]",
+				BuildConfig.VERSION_NAME));
+	}
 
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return true;
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		return super.onOptionsItemSelected(item);
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        finish();
-    }
+	@Override
+	protected void onPause() {
+		super.onPause();
+		finish();
+	}
 }
